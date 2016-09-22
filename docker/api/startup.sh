cb69588f1c6c4064375aad6fe2b356855ae9a23c
Nic Barker <bicnarker@me.com>
2016-09-23 09:48:21 +1000

    Fixed some bugs with the database name and port binding, updated backend commit ref

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -2,8 +2,8 @@
 # Run the initial sqitch migrations
 sleep 10 &&
 cd ./sqitch &&
-/usr/local/bin/sqitch deploy db:pg://spirit_api:spirit_api@postgres:5432/spirit_api &&
-PGPASSWORD=spirit_api psql -h postgres -U spirit_api --file=/root/seed.sql &&
+/usr/local/bin/sqitch deploy db:pg://spirit_api:spirit_api@postgres:5432/spirit &&
+PGPASSWORD=spirit_api psql -h postgres -U spirit_api -d spirit --file=/root/seed.sql &&
 cd /spirit/spirit-api &&
 # Run node with PM2
 ./node_modules/.bin/pm2 start /spirit/docker/api/pm2.config.json --no-daemon
